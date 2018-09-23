d7efde5a890e8e0878f761a90a80b6189bb12fd3
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-23 02:19:55 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,39 +10,40 @@ def actor
 end
 
 def monopoly
-	monopoly = {}
-
+	monopoly = {
+		:railroads =>
+	}
 end
 
 def monopoly_with_second_tier
 	monopoly = {
-		:railroads=>
+		:railroads =>
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :pieces => 4,
+    :rent_in_dollars =>
     {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
     },
-    :names=>
+    :names =>
     {
-      :reading_railroad=>
+      :reading_railroad =>
       {
-        "mortgage_value"=>"$100"
+        "mortgage_value" => "$100"
       },
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad =>
       {
-        "mortgage_value"=>"$200"
+        "mortgage_value"  =>"$200"
       },
-      :b_and_o_railroad=>
+      :b_and_o_railroad =>
       {
-        "mortgage_value"=>"$400"
+        "mortgage_value" => "$400"
       },
       :shortline=>
       {
-        "mortgage_value"=>"$800"
+        "mortgage_value" => "$800"
       }
     }
   }
@@ -55,6 +56,36 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+	monopoly = {
+		:railroads =>
+	{
+		:pieces => 4,
+		:rent_in_dollars =>
+		{
+			:one_piece_owned => 25,
+			:two_pieces_owned => 50,
+			:three_pieces_owned => 100,
+			:four_pieces_owned => 200
+		},
+		:names =>
+		{
+			:reading_railroad =>
+			{
+				"mortgage_value" => "$100"
+			},
+			:pennsylvania_railroad =>
+			{
+				"mortgage_value"  =>"$200"
+			},
+			:b_and_o_railroad =>
+			{
+				"mortgage_value" => "$400"
+			},
+			:shortline=>
+			{
+				"mortgage_value" => "$800"
+			}
+		}
+	}
+	}
 end
